65a5738e94a6b4d3bd32e19a7f534b1b4dc89e03
Northburns <northburns@gmail.com>
2021-02-20 22:00:53 +0200

    start with some small example pixi

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import _ from 'lodash';
+import * as PIXI from 'pixi.js';
 
 function component() {
-    const element = document.createElement('div');
+    const app = new PIXI.Application({
+        width: 800,
+        height: 600,
+        backgroundColor: 0x1099bb,
+        resolution: window.devicePixelRatio || 1,
+    });
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const texture = PIXI.Texture.from('imgs/parrot.png');
+    const container = new PIXI.Container();
+    app.stage.addChild(container);
 
-    return element;
+    for (let i = 0; i < 25; i++) {
+        const bunny = new PIXI.Sprite(texture);
+        bunny.anchor.set(0.5);
+        bunny.x = (i % 5) * 40;
+        bunny.y = Math.floor(i / 5) * 40;
+        container.addChild(bunny);
+    }
+
+    container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+
+// Center bunny sprite in local container coordinates
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+// Listen for animate update
+app.ticker.add((delta) => {
+    // rotate the container!
+    // use delta to create frame-independent transform
+    container.rotation -= 0.01 * delta;
+});
+
+    
+    return app.view;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'docs'),
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'docs'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+		new CopyWebpackPlugin({
+      patterns: [
+        { from: "assets", to: "." }
+      ],
+    })
+	]
 };
