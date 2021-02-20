49fd3c4ab4d5dfc0d71a14ff4803e3c4fa2a186b
Northburns <northburns@gmail.com>
2021-02-21 00:22:31 +0200

    trying out the examples

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import * as PIXI from 'pixi.js';
+global.PIXI = PIXI;
+require("pixi-projection");
 
 function component() {
     const app = new PIXI.Application({
@@ -10,30 +12,59 @@ function component() {
     });
 
     const texture = PIXI.Texture.from('imgs/parrot.png');
-    const container = new PIXI.Container();
-    app.stage.addChild(container);
 
-    for (let i = 0; i < 25; i++) {
-        const bunny = new PIXI.Sprite(texture);
-        bunny.anchor.set(0.5);
-        bunny.x = (i % 5) * 40;
-        bunny.y = Math.floor(i / 5) * 40;
-        container.addChild(bunny);
-    }
+    const camera = new PIXI.projection.Camera3d();
+    camera.setPlanes(300, 10, 1000, false);
+    camera.position.set(app.screen.width / 2, 0);
+    camera.position3d.y = -500; // camera is above the ground
+    app.stage.addChild(camera);
 
-    container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+    const groundLayer = new PIXI.projection.Container3d();
+    groundLayer.euler.x = Math.PI / 2;
+    camera.addChild(groundLayer);
 
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+    // Those two layers can have 2d objects inside
+    // because they return everything to affine space
 
-// Listen for animate update
-app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
-});
+    const bgLayer = new PIXI.projection.Container3d();
+    bgLayer.proj.affine = PIXI.projection.AFFINE.AXIS_X;
+    camera.addChild(bgLayer);
+    bgLayer.position3d.z = 80;
+
+    const mainLayer = new PIXI.projection.Container3d();
+    mainLayer.proj.affine = PIXI.projection.AFFINE.AXIS_X;
+    camera.addChild(mainLayer);
+
+    // FOOO
+    const f = new PIXI.projection.Sprite3d(texture);
+    groundLayer.addChild(f);
+
+
+    // const texture = PIXI.Texture.from('imgs/parrot.png');
+    // const container = new PIXI.Container();
+    // app.stage.addChild(container);
+
+    // for (let i = 0; i < 25; i++) {
+    //     const bunny = new PIXI.Sprite(texture);
+    //     bunny.anchor.set(0.5);
+    //     bunny.x = (i % 5) * 40;
+    //     bunny.y = Math.floor(i / 5) * 40;
+    //     container.addChild(bunny);
+    // }
+
+    // container.x = app.screen.width / 2;
+    // container.y = app.screen.height / 2;
+
+    // // Center bunny sprite in local container coordinates
+    // container.pivot.x = container.width / 2;
+    // container.pivot.y = container.height / 2;
+
+    // // Listen for animate update
+    // app.ticker.add((delta) => {
+    //     // rotate the container!
+    //     // use delta to create frame-independent transform
+    //     container.rotation -= 0.01 * delta;
+    // });
 
     
     return app.view;
